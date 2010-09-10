8b53ee7b407f224cfad48ee458e6b7cb6192269f
Martin Rusev <martinrusev@zoho.com>
2010-09-10 17:55:45 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_redis_sessions',
+    version='0.1.0',
+    description="Redis Session Backend For Django",
+    long_description="",
+    keywords='django, sessions,',
+    author='Martin Rusev',
+    author_email='martinmcloud@gmail.com',
+    url='http://github.com/martinrusev/django-redis-sessions',
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['setuptools','redis',],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+) 
+
+
