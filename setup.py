919dad87776a90c912edf71eaa8096882dc20118
Dan Loewenherz <dloewenherz@gmail.com>
2012-03-13 10:32:03 -0700

    remove setuptools from requirements
    Setup.py can't even run if setuptools is not installed, so it's an assumed
    requirement. It also creates potentially undesirable side-effects, since
    running `pip install django-redis-sessions --upgrade` will upgrade setuptools
    as well.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     license='BSD',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=['setuptools', 'redis', ],
+    install_requires=['redis', ],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python",
