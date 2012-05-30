ba291625ed9e0ed796a40f4b2744bb0f07ac5b88
jeffbaier <jbaier@nex-tech.com>
2012-05-30 09:09:06 -0500

    Fixed final issues from SessionBase.session_key read only problems.
    Refs:
    https://github.com/django/django/commit/bda21e2b9d
    https://code.djangoproject.com/ticket/13478

--- redis_sessions/session.py
+++ redis_sessions/session.py
@@ -19,7 +19,7 @@ class SessionStore(SessionBase):
 
     def load(self):
         try:
-            session_data = self.server.get(self.get_real_stored_key(self.session_key))
+            session_data = self.server.get(self.get_real_stored_key(self._get_or_create_session_key()))
             return self.decode(force_unicode(session_data))
         except:
             self.create()
@@ -40,17 +40,17 @@ class SessionStore(SessionBase):
             return
 
     def save(self, must_create=False):
-        if must_create and self.exists(self.session_key):
+        if must_create and self.exists(self._get_or_create_session_key()):
             raise CreateError
         data = self.encode(self._get_session(no_load=must_create))
-        self.server.set(self.get_real_stored_key(self.session_key), data)
-        self.server.expire(self.get_real_stored_key(self.session_key), self.get_expiry_age())
+        self.server.set(self.get_real_stored_key(self._get_or_create_session_key()), data)
+        self.server.expire(self.get_real_stored_key(self._get_or_create_session_key()), self.get_expiry_age())
 
     def delete(self, session_key=None):
         if session_key is None:
-            if self._session_key is None:
+            if self.session_key is None:
                 return
-            session_key = self._session_key
+            session_key = self.session_key
         try:
             self.server.delete(self.get_real_stored_key(session_key))
         except:
