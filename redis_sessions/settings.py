9c87a6889a0dc4ecf0c1034243c344022b2e32bf
hellysmile <hellysmile@gmail.com>
2013-02-27 01:27:16 +0200

    Fix redis prefix for existing sessions

--- redis_sessions/settings.py
+++ redis_sessions/settings.py
@@ -4,7 +4,7 @@ from django.conf import settings
 SESSION_REDIS_HOST = getattr(settings, 'SESSION_REDIS_HOST', 'localhost')
 SESSION_REDIS_PORT = getattr(settings, 'SESSION_REDIS_PORT', 6379)
 SESSION_REDIS_DB = getattr(settings, 'SESSION_REDIS_DB', 0)
-SESSION_REDIS_PREFIX = getattr(settings, 'SESSION_REDIS_PREFIX', 'session')
+SESSION_REDIS_PREFIX = getattr(settings, 'SESSION_REDIS_PREFIX', '')
 SESSION_REDIS_PASSWORD = getattr(settings, 'SESSION_REDIS_PASSWORD', None)
 SESSION_REDIS_UNIX_DOMAIN_SOCKET_PATH = getattr(
     settings, 'SESSION_REDIS_UNIX_DOMAIN_SOCKET_PATH', None
