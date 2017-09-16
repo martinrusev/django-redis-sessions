c57f6c7980c3ce1c35b77c1e36e9e7c3a1014f37
martinrusev <martin@amon.cx>
2017-09-16 10:03:16 +0200

    Update

--- redis_sessions/settings.py
+++ redis_sessions/settings.py
@@ -1,18 +1,18 @@
 from django.conf import settings
 
 # SESSION_REDIS - Default
-SESSION_REDIS = getattr(settings, 'SESSION_REDIS', {})
+SESSION_REDIS = settings.get('SESSION_REDIS', {})
 
 
-SESSION_REDIS_HOST = getattr(SESSION_REDIS, 'host', 'localhost')
-SESSION_REDIS_PORT = getattr(SESSION_REDIS, 'port', 6379)
-SESSION_REDIS_SOCKET_TIMEOUT = getattr(SESSION_REDIS, 'socket_timeout', 0.1)
-SESSION_REDIS_RETRY_ON_TIMEOUT = getattr(SESSION_REDIS, 'retry_on_timeout', False)
-SESSION_REDIS_DB = getattr(SESSION_REDIS, 'db', 0)
-SESSION_REDIS_PREFIX = getattr(SESSION_REDIS, 'prefix', '')
-SESSION_REDIS_PASSWORD = getattr(SESSION_REDIS, 'password', None)
-SESSION_REDIS_UNIX_DOMAIN_SOCKET_PATH = getattr(SESSION_REDIS, 'unix_domain_socket_path', None)
-SESSION_REDIS_URL = getattr(SESSION_REDIS, 'url', None)
+SESSION_REDIS_HOST = SESSION_REDIS.get('host', 'localhost')
+SESSION_REDIS_PORT = SESSION_REDIS.get('port', 6379)
+SESSION_REDIS_SOCKET_TIMEOUT = SESSION_REDIS.get('socket_timeout', 0.1)
+SESSION_REDIS_RETRY_ON_TIMEOUT = SESSION_REDIS.get('retry_on_timeout', False)
+SESSION_REDIS_DB = SESSION_REDIS.get('db', 0)
+SESSION_REDIS_PREFIX = SESSION_REDIS.get('prefix', '')
+SESSION_REDIS_PASSWORD = SESSION_REDIS.get('password', None)
+SESSION_REDIS_UNIX_DOMAIN_SOCKET_PATH = SESSION_REDIS.get('unix_domain_socket_path', None)
+SESSION_REDIS_URL = SESSION_REDIS.get('url', None)
 
 """
 Should be on the format:
@@ -37,8 +37,8 @@ Should be on the format:
     },
 ]
 """
-SESSION_REDIS_POOL = getattr(SESSION_REDIS, 'POOL', None)
+SESSION_REDIS_POOL = SESSION_REDIS.get('POOL', None)
 
 # should be on the format [(host, port), (host, port), (host, port)]
-SESSION_REDIS_SENTINEL_LIST = getattr(settings, 'SESSION_REDIS_SENTINEL_LIST', None)
-SESSION_REDIS_SENTINEL_MASTER_ALIAS = getattr(settings, 'SESSION_REDIS_SENTINEL_MASTER_ALIAS', None)
+SESSION_REDIS_SENTINEL_LIST = settings.get('SESSION_REDIS_SENTINEL_LIST', None)
+SESSION_REDIS_SENTINEL_MASTER_ALIAS = settings.get('SESSION_REDIS_SENTINEL_MASTER_ALIAS', None)
